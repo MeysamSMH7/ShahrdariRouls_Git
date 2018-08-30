1db923ed290b62eefeaa4971d281ea6891b2985f
MeysamSMH7 <meysamsm@yahoo.com>
2018-08-30 16:19:33 +0430

    added these bullsheits

--- app/src/main/java/com/app/shahrdarirouls/Activity_Favorite.java
+++ app/src/main/java/com/app/shahrdarirouls/Activity_Favorite.java
@@ -1,19 +1,12 @@
 package com.app.shahrdarirouls;
 
-import android.app.Activity;
-import android.app.Application;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,7 +15,6 @@ import android.widget.Toast;
 import com.app.shahrdarirouls.DataBase.DataSource.tb_FavoriteDATASource;
 import com.app.shahrdarirouls.DataBase.Table.tb_Favorite;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Activity_Favorite extends AppCompatActivity {
@@ -42,6 +34,7 @@ ArrayAdapter arrayAdapter;
         data = tb_favoriteDATASource.GetList();
         tb_favoriteDATASource.Close();
 
+// kossher
 
         lstFavorite = (ListView) findViewById(R.id.lstFavorite);
 
